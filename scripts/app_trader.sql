e3b3cc2e14c4b1683561e70da014fd50af2b7f60
janvimehendale <janvimehendale@gmail.com>
2025-10-16 23:18:25 -0500

    Working on Marketing cost

--- scripts/app_trader.sql
+++ scripts/app_trader.sql
@@ -28,6 +28,15 @@ SELECT name, price
 FROM play_store_apps
 WHERE name ILIKE '%Rich - Trump Edition%';
 */
+-------------------------------------------------------------------------------------------------
+-- a. App Trader will purchase apps for 10,000 times the price of the app. 
+--For apps that are priced from free up to $1.00, the purchase price is $10,000.
+-- For example, an app that costs $2.00 will be purchased for $20,000.
+-- The cost of an app is not affected by how many app stores it is on. A $1.00 app on the 
+--Apple app store will cost the same as a $1.00 app on both stores. 
+-- If an app is on both stores, it's purchase price will be calculated based off of the 
+--highest app price between the two stores. 
+
 
 WITH highest_price_cte AS (--to calculate higher of app or play store price
 	SELECT
@@ -59,28 +68,38 @@ ORDER BY purchase_price DESC),
 --SELECT
 --	*
 --FROM purchase_price_cte
+---------------------------------------------------------------------------------
+-- b. Apps earn $5000 per month, per app store it is on, from in-app advertising and in-app purchases,
+--regardless of the price of the app.
+-- An app that costs $200,000 will make the same per month as an app that costs $1.00. 
+-- An app that is on both app stores will make $10,000 per month. 
 
 earnings_cte AS (
 	SELECT *,
-	CASE WHEN a.name = p.name THEN 10000
-		 ELSE 5000
-		 END AS earning_per_month
-	FROM purchase_price_cte
-	
-		)
-
-SELECT
+			CASE 
+				WHEN app_store_name IS NOT NULL AND play_store_name IS NOT NULL THEN 10000
+		 		ELSE 5000
+		 	END AS earning_per_month
+		 FROM purchase_price_cte
+			)
+		
+SELECT 
 	*
 FROM earnings_cte
 	
 	
-	
+---------------------------------------------------------------------------------
+-- c. App Trader will spend an average of $1000 per month to market an app regardless of the price of
+--the app. If App Trader owns rights to the app in both stores, it can market the app for both stores
+--for a single cost of $1000 per month.
+-- An app that costs $200,000 and an app that costs $1.00 will both cost $1000 a month for marketing,
+--regardless of the number of stores it is in.
 
+marketing_cost_cte AS(
+
+)
 ---------------------------------------------------------------------------------
---WHERE name IN (a.name, p.name)	--price per month 10000
---ELSE - earn price per 5000
----------------------------------------------------------------------------------
---  For every half point that an app gains in rating, its projected lifespan increases by one year. 
+-- d. For every half point that an app gains in rating, its projected lifespan increases by one year. 
 --In other words, an app with a rating of 0 can be expected to be in use for 1 year, an app with a
 --rating of 1.0 can be expected to last 3 years, and an app with a rating of 4.0 can be expected to 
 --last 9 years.
@@ -88,19 +107,23 @@ FROM earnings_cte
 -- - App store ratings should be calculated by taking the average of the scores from both app stores
 --and rounding to the nearest 0.5.
 
-WITH combined_rating_cte AS(
+WITH avg_rating_cte AS(
 	SELECT
 		a.name,
 		p.name,
 		COALESCE(a.rating,0) AS app_store_rating,
-		COALESCE(p.rating,0) AS play_store_rating
+		COALESCE(p.rating,0) AS play_store_rating,
+		ROUND(ROUND(COALESCE(a.rating,0) + COALESCE(p.rating,0),0)/2,2) AS avg_ratings
 	FROM app_store_apps a
 	FULL JOIN play_store_apps p
 	USING (name)
+	GROUP BY a.name, p.name, a.rating, p.rating
 		)
 
 SELECT
 	* 
-FROM combined_rating_cte
+FROM avg_rating_cte
 
 ---------------------------------------------------------------------------------
+--e. App Trader would prefer to work with apps that are available in both the App Store and the Play Store since they can market both for the same
+--$1000 per month.
